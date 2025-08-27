eefaba7c5d5a16804cebbd55830c58ff79cc06d0
mahidhartatipakala <mahidhartatipakala@gmail.com>
2025-08-27 17:13:01 +0000

    --update

---
+++ script.js
@@ -0,0 +1,15 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('main > div');
+
+    sections.forEach(section => {
+        section.classList.add('hidden');
+    });
+
+    const activeSection = document.getElementById(sectionId);
+    if (activeSection) {
+        activeSection.classList.remove('hidden');
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    showSection('welcome-section');
+});
